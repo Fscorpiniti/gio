8e8686deccc555934e76b73c2cea1717b577d209
fscorpiniti <fernando.scorpiniti@gmail.com>
2016-10-10 18:39:55 -0300

    Agrego nuevo test del repositorio relacional

--- src/test/java/ar/edu/untref/gio/persistence/RelationalUserRepositoryTest.java
+++ src/test/java/ar/edu/untref/gio/persistence/RelationalUserRepositoryTest.java
@@ -12,19 +12,27 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
+@Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:application-context-test.xml"})
-@Transactional
 public class RelationalUserRepositoryTest {
+
+    private static final String VALID_EMAIL = "test@gio.com";
+    private static final String VALID_PASSWORD = "auth";
 
     @Resource(name = "relationalUserRepository")
     private UserRepository userRepository;
 
     @Test
     public void whenCreateUserThenUserIsStored() {
-        User user = new User("test@gio.com", "auth", new DefaultUserValidator());
+        User user = new User(VALID_EMAIL, VALID_PASSWORD, new DefaultUserValidator());
         userRepository.add(user);
         Assert.assertTrue(userRepository.exist(user.getEmail()));
+    }
+
+    @Test
+    public void whenEmailIsInexistentThenResultIsFalse() {
+        Assert.assertFalse(userRepository.exist(VALID_EMAIL));
     }
 
 }
