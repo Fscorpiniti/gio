b195840f87d4f51d8ba55ba8b004da209f32787b
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-05-14 21:48:22 -0300

    Devuelvo el token en el header

--- src/main/java/ar/edu/untref/gio/presentation/controller/LoginController.java
+++ src/main/java/ar/edu/untref/gio/presentation/controller/LoginController.java
@@ -2,6 +2,7 @@ package ar.edu.untref.gio.presentation.controller;
 
 import ar.edu.untref.gio.domain.User;
 import ar.edu.untref.gio.domain.request.LoginRequest;
+import ar.edu.untref.gio.domain.service.CreateTokenService;
 import ar.edu.untref.gio.presentation.response.UserResponse;
 import ar.edu.untref.gio.presentation.response.UserResponseFactory;
 import ar.edu.untref.gio.domain.interactor.AuthenticationInteractor;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import java.util.Optional;
 
@@ -18,14 +20,21 @@ import java.util.Optional;
 @Transactional
 public class LoginController {
 
+    private static final String AUTH_TOKEN = "auth_token";
+
     @Resource(name = "authenticationInteractor")
     private AuthenticationInteractor authenticationInteractor;
+
+    @Resource(name = "createTokenService")
+    private CreateTokenService createTokenService;
 
     @ResponseBody
     @ApiOperation(value = "Login")
     @RequestMapping(value =  "/login", method = RequestMethod.POST,  consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public UserResponse login(@RequestBody LoginRequest loginRequest) {
-        Optional<User> user = this.authenticationInteractor.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+    public UserResponse login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        Optional<User> user = this.authenticationInteractor.authenticate(loginRequest.getEmail(),
+                loginRequest.getPassword());
+        response.setHeader(AUTH_TOKEN, createTokenService.create(user.get().getId()));
         return new UserResponseFactory().build(user.get());
     }
 
