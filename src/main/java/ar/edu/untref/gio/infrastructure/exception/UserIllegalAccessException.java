a5e00486b27556415c0d32204c82976e0297040e
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-05-15 09:48:12 -0300

    Mensaje a la excepcion

--- src/main/java/ar/edu/untref/gio/infrastructure/exception/UserIllegalAccessException.java
+++ src/main/java/ar/edu/untref/gio/infrastructure/exception/UserIllegalAccessException.java
@@ -1,4 +1,11 @@
 package ar.edu.untref.gio.infrastructure.exception;
 
 public class UserIllegalAccessException extends RuntimeException {
+
+    private static final String ILLEGAL_ACCESS = "Illegal access";
+
+    public UserIllegalAccessException() {
+        super(ILLEGAL_ACCESS);
+    }
+
 }
